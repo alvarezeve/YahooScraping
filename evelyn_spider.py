d980b05b2c482958a04f817190f283954da47630
alvarezeve <evelyn_333@ciencias.unam.mx>
2019-02-06 20:14:54 -0600

    funciona y produce un archivo de salida

--- evelyn_spider.py
+++ evelyn_spider.py
@@ -1,8 +1,7 @@
-#..........................................Spider_Excercise....................................
+#....................................Extract data from Yahoo Finance....................................
 #_______________________________________This is a test for Theo________________________________
 
-
-
+#Import library
 from lxml import html
 import requests
 from time import sleep
@@ -11,32 +10,36 @@ import argparse
 from collections import OrderedDict
 from time import sleep
 
+
+#definition of parse (analizer), the method parse analize the url yahoo finance
+# for each case of item (VTV,VOE,VTI, etc.)
 def parse(ticker):
-	url = "http://finance.yahoo.com/quote/%s?p=%s"%(ticker,ticker)
-	response = requests.get(url, verify=False)
-	print ("Parsing %s"%(url))
-	sleep(4)
-	parser = html.fromstring(response.text)
-	summary_table = parser.xpath('//div[contains(@data-test,"summary-table")]//tr')
-	summary_data = OrderedDict()
+	#https://finance.yahoo.com/quote/SCHF?p=SCHF&.tsrc=fin-srch
+	url = "http://finance.yahoo.com/quote/%s?p=%s&.tsrc=fin-srch"%(ticker,ticker) #pass the url
+	response = requests.get(url, verify=False) # get the information from object request
+	print ("Parsing %s"%(url)) #show a url for analizer
+	sleep(4) #make a pause only for get a time
+	parser = html.fromstring(response.text) #from object response using atribute text for extract string
+	summary_table = parser.xpath('//div[contains(@data-test,"summary-table")]//tr') #give the path
+	summary_data = OrderedDict() #for remember the order in dictionary
 	other_details_json_link = "https://query2.finance.yahoo.com/v10/finance/quoteSummary/{0}?formatted=true&lang=en-US&region=US&modules=summaryProfile%2CfinancialData%2CrecommendationTrend%2CupgradeDowngradeHistory%2Cearnings%2CdefaultKeyStatistics%2CcalendarEvents&corsDomain=finance.yahoo.com".format(ticker)
 	summary_json_response = requests.get(other_details_json_link)
 	try:
+		#print ("Parsing %s"%(url))
 		json_loaded_summary =  json.loads(summary_json_response.text)
-		y_Target_Est = json_loaded_summary["quoteSummary"]["result"][0]["financialData"]["targetMeanPrice"]['raw']
-		earnings_list = json_loaded_summary["quoteSummary"]["result"][0]["calendarEvents"]['earnings']
-		eps = json_loaded_summary["quoteSummary"]["result"][0]["defaultKeyStatistics"]["trailingEps"]['raw']
+		#print (json_loaded_summary["quoteSummary"]["result"][0])
+		eps = json_loaded_summary["quoteSummary"]["result"][0]["defaultKeyStatistics"]
 		datelist = []
-		for i in earnings_list['earningsDate']:
-			datelist.append(i['fmt'])
-		earnings_date = ' to '.join(datelist)
+		#print ("Parsing %s"%(url))
 		for table_data in summary_table:
 			raw_table_key = table_data.xpath('.//td[contains(@class,"C(black)")]//text()')
+		#	print(raw_table_key)
 			raw_table_value = table_data.xpath('.//td[contains(@class,"Ta(end)")]//text()')
+		#	print(raw_table_value)
 			table_key = ''.join(raw_table_key).strip()
 			table_value = ''.join(raw_table_value).strip()
 			summary_data.update({table_key:table_value})
-		summary_data.update({'1y Target Est':y_Target_Est,'EPS (TTM)':eps,'Earnings Date':earnings_date,'ticker':ticker,'url':url})
+		#	print(summary_data)
 		return summary_data
 	except:
 		print ("Failed to parse json response")
