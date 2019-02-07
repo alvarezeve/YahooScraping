294c73cf9db19ef51843306731fd3bba96e7c0fa
alvarezeve <evelyn_333@ciencias.unam.mx>
2019-02-07 17:04:15 -0600

    version final, para enviar

--- evelyn_spider.py
+++ evelyn_spider.py
@@ -1,5 +1,5 @@
 #....................................Extract data from Yahoo Finance....................................
-#_______________________________________This is a test for Theo________________________________
+#_______________________________________This is a test ________________________________
 
 #Import library
 from lxml import html
@@ -11,65 +11,49 @@ from collections import OrderedDict
 from time import sleep
 
 from sqlalchemy import create_engine
-#from sqlalchemy import update
-#from sqlalchemy import create_engine
-#from sqlalchemy.ext.declarative import declarative_base
-#from sqlalchemy.orm import sessionmaker
-#from sqlalchemy import create_engine, MetaData, Table
-#from sqlalchemy.orm import mapper, sessionmaker
+from sqlalchemy import update
 
-
-
-#definition of parse (analizer), the method parse analize the url yahoo finance
-# for each case of item (VTV,VOE,VTI, etc.)
+# for analizer the url using a parse method, works one by one (VTV,VOE,...)
 def parse(ticker):
-	#https://finance.yahoo.com/quote/SCHF?p=SCHF&.tsrc=fin-srch
 	url = "http://finance.yahoo.com/quote/%s?p=%s&.tsrc=fin-srch"%(ticker,ticker) #pass the url
 	response = requests.get(url, verify=False) # get the information from object request
-	print ("Parsing %s"%(url)) #show a url for analizer
-	sleep(4) #make a pause only for get a time
-	parser = html.fromstring(response.text) #from object response using atribute text for extract string
-	summary_table = parser.xpath('//div[contains(@data-test,"summary-table")]//tr') #give the path
-	summary_data = OrderedDict() #for remember the order in dictionary
+	print ("Parsing %s"%(url)) #show a url for analizer, only format
+	sleep(4) #make a pause only for get a time in the explorer
+	parser = html.fromstring(response.text) #from object response using atribute text
+	summary_table = parser.xpath('//div[contains(@data-test,"summary-table")]//tr') #give the Xpath to the content
+	summary_data = OrderedDict() #for remember the order in dictionary and create a summary
 	other_details_json_link = "https://query2.finance.yahoo.com/v10/finance/quoteSummary/{0}?formatted=true&lang=en-US&region=US&modules=summaryProfile%2CfinancialData%2CrecommendationTrend%2CupgradeDowngradeHistory%2Cearnings%2CdefaultKeyStatistics%2CcalendarEvents&corsDomain=finance.yahoo.com".format(ticker)
-	summary_json_response = requests.get(other_details_json_link)
+	summary_json_response = requests.get(other_details_json_link) #get more info
 	try:
-		#print ("Parsing %s"%(url))
-		json_loaded_summary =  json.loads(summary_json_response.text)
-		#print (json_loaded_summary["quoteSummary"]["result"][0])
-		eps = json_loaded_summary["quoteSummary"]["result"][0]["defaultKeyStatistics"]
+		json_loaded_summary =  json.loads(summary_json_response.text) #try to extract text from sumary
+		eps = json_loaded_summary["quoteSummary"]["result"][0]["defaultKeyStatistics"] #exctract statistics info
 		datelist = []
-		#print ("Parsing %s"%(url))
 		for table_data in summary_table:
-			raw_table_key = table_data.xpath('.//td[contains(@class,"C(black)")]//text()')
-		#	print(raw_table_key)
-			raw_table_value = table_data.xpath('.//td[contains(@class,"Ta(end)")]//text()')
-			#print(raw_table_value[0])
+			raw_table_key = table_data.xpath('.//td[contains(@class,"C(black)")]//text()') #for read infor from begin
+			raw_table_value = table_data.xpath('.//td[contains(@class,"Ta(end)")]//text()') #at the end
 			table_key = ''.join(raw_table_key).strip()
 			table_value = ''.join(raw_table_value).strip()
-			summary_data.update({table_key:table_value})
-		#	print(summary_data)
+			summary_data.update({table_key:table_value}) #join or link the data
 		return summary_data
-		#print(raw_table_value)
 	except:
 		print ("Failed to parse json response")
 		return {"error":"Failed to parse json response"}
 
 
 
-
+#now we do everything
 if __name__=="__main__":
 	argparser = argparse.ArgumentParser()
-	argparser.add_argument('ticker',help = '')
+	argparser.add_argument('ticker',help = '') #for read and help the user
 	args = argparser.parse_args()
-	ticker = args.ticker
-	print ("Fetching data for %s"%(ticker))
-	scraped_data = parse(ticker)
-	print ("Writing data to output file")
+	ticker = args.ticker #value of active
+	print ("Fetching data for %s"%(ticker)) #only format
+	scraped_data = parse(ticker) #using the parse method
+	print ("Writing data to output file") #Ready!
 	with open('%s-summary.json'%(ticker),'w') as fp:
 		json.dump(scraped_data,fp,indent = 4)
-
-PC = scraped_data.items()[0][1]
+#for actualize the table called FinanceY. The actives are rows in FinanceY and the financel data are columns
+PC = scraped_data.items()[0][1] #asign a var for each data
 OP = scraped_data.items()[1][1]
 DR = scraped_data.items()[4][1]
 WR52 = scraped_data.items()[5][1]
@@ -79,61 +63,22 @@ PER = scraped_data.items()[10][1]
 Y = scraped_data.items()[11][1]
 D = scraped_data.items()[15][1]
 
-print(PC,OP,DR,WR52,V,NAV_V,PER,Y,D)
+db_string = "postgresql://postgres:evelyn@localhost:5432/postgres" #path for my data base
 
+db = create_engine(db_string) #object called db because is data base
+#one by one only because is to large and unconfortable in one line
+db.execute("UPDATE FinanceY SET PreviousClose=%s WHERE Activo=%s",(PC,ticker)) #update the database
+db.execute("UPDATE FinanceY SET OpenPrice=%s WHERE Activo=%s",(OP,ticker))
+db.execute("UPDATE FinanceY SET DaysRange=%s WHERE Activo=%s",(DR,ticker))
+db.execute("UPDATE FinanceY SET WeekRange52=%s WHERE Activo=%s",(PC,ticker))
+db.execute("UPDATE FinanceY SET Volume=%s WHERE Activo=%s",(V,ticker))
+db.execute("UPDATE FinanceY SET NAV=%s WHERE Activo=%s",(NAV_V,ticker))
+db.execute("UPDATE FinanceY SET PE_Ratio=%s WHERE Activo=%s",(PER,ticker))
+db.execute("UPDATE FinanceY SET FechaConsulta=%s WHERE Activo=%s",(D,ticker))
+db.execute("UPDATE FinanceY SET Yield=%s WHERE Activo=%s",(Y,ticker))
 
-#db_string = create_engine('postgresql://usr:pass@localhost:5432/sqlalchemy')
-db_string = "postgresql://postgres:evelyn@localhost:5432/postgres"
-
-db = create_engine(db_string)
-print("a")
-
-# Create
-#db.execute("CREATE TABLE IF NOT EXISTS films (title text, director text, year text)")
-db.execute("UPDATE NameFinance SET PreviousClose=OP WHERE ACTIVO='VTV'")
-#, OpenPrice=scraped_data.items()[1][1], DaysRange=scraped_data.items()[4][1],WeekRange52=scraped_data.items()[5][1],Volume=scraped_data.items()[6][1], NAV=scraped_data.items()[9][1],PE_Ratio=scraped_data.items()[10][1],Yield=scraped_data.items()[11][1],ConsultDate=scraped_data.items()[15][1] WHERE Activo='%s')
-print("b")
 # Read
-result_set = db.execute("SELECT * FROM NAmeFinance")
+result_set = db.execute("SELECT * FROM FinanceY")
 for r in result_set:
     print(r)
-
-# Update
-#db.execute("UPDATE films SET title='Some2016Film' WHERE year='2016'")
-
-# Delete
-#db.execute("DELETE FROM films WHERE year='2016'")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#connection = sqlite3.connect("company.db")
-#!/usr/bin/python
-#engine = create_engine('postgresql://usr:pass@localhost:5432/sqlalchemy')
-
-
-#class YahooTable(object):
-#    pass
-
-
-
-#stmt = update(activos).where(Activo='%s').\
-#    values(PreviousClose=scraped_data.items()[0][1])
-#,OpenPrice=scraped_data.items()[1][1],DaysRange=scraped_data.items()[4][1]),WeekRange52=scraped_data.items()[5][1],Volume=scraped_data.items()[6][1],NAV=scraped_data.items()[9][1],PE Ratio = scraped_data.items()[10][1],Yield=scraped_data.items()[11][1],ConsultDate=scraped_data.items()[15][1]);
-#cursor = conn.cursor()
-#cursor.execute("UPDATE activos", (v1, v2, v3))
-#conn.commit() # <- We MUST commit to reflect the inserted data
-#cursor.close()
-#conn.close()
+#end
